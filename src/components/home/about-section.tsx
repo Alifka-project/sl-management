0ddce4fdc55abb7d31729e5c4e641f665af1421f
Alifka-project <alifka.iqbal@gmail.com>
2025-06-25 00:48:28 +0400

    Update about-section.tsx

--- src/components/home/about-section.tsx
+++ src/components/home/about-section.tsx
@@ -37,12 +37,12 @@ export default function AboutSection() {
               {t('about.title')}
             </h2>
             <FadeIn direction='up' delay={0.6}>
-              <p className='text-[#252525] text-base sm:text-lg md:text-2xl leading-relaxed transform transition-transform duration-200'>
+              <p className='text-[#252525] text-base sm:text-lg md:text-2xl leading-relaxed transform transition-transform duration-200 text-justify'>
                 {t('about.description1')}
               </p>
             </FadeIn>
             <FadeIn direction='up' delay={0.8}>
-              <p className='text-[#252525] text-base sm:text-lg md:text-2xl leading-relaxed transform transition-transform duration-200'>
+              <p className='text-[#252525] text-base sm:text-lg md:text-2xl leading-relaxed transform transition-transform duration-200 text-justify'>
                 {t('about.description2')}
               </p>
             </FadeIn>
